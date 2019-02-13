821a147edaaa8b6e98b5b5d8b92b678990279e87
Don Smith <don@antiantonym.com>
2019-02-13 16:15:06 +1300

    update all dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,13 @@ const path = require('path')
 
 module.exports = {
   entry: './client/index.js',
+  mode: 'development',
   output: {
     path: path.join(__dirname, '/public'),
     filename: 'bundle.js'
   },
   module: {
-    loaders: [
+    rules: [
       {
         loader: 'babel-loader',
         test: /\.jsx?$/,
